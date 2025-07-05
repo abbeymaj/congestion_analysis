c12f38c5f6c4c185cc1982b602f117579e404448
abbeymaj <abbey.majumdar@gmail.com>
2025-07-05 12:36:05 -0400

    application file creation

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 ## Congestion Analysis
+
+The objective of this project is to predict the traffic congestion, given the time of day, coordinates and direction. This problem is a regression problem.
+
+## Installation
+
+Install the project using pip. It is always recommended to use a virtual environment (for example, using anaconda) to do the installation.
+
+This project was built using Python 3.9.
+
+To install the project, use the following: 
+
+```bash
+  pip install -r requirements.txt
+```
+    
+## Tech Stack
+
+**Client:** Flask, HTML, CSS
+
+**Language:** Python
+
+**Model Registry:** MLflow
+
+## Screenshots of the Prediction Website 
+
+There are two webpages, which the user can interact with, for the project. 
+
+Landing Page:
+
+This is the landing page for the project. The page provides a brief overview of the project and then allows the user to navigate to the prediction page. 
+
+The screenshot of the Landing Page is given below:
+
+![Landing Page Screenshot Link](https://github.com/abbeymaj80/my-ml-datasets/blob/master/screenshots/Congestion_Landing_Page.png)
+
+Prediction Page:
+
+The prediction page enables the user to select or enter information that describes coordinates and direction in order for the application to predict traffic congestion. Once the user enters the necessary information, the user must click on the "Predict" button to generate the congestion prediction.
+
+The screenshot of the Prediction page is given below:
+
+![Prediction page Screenshot Link](https://github.com/abbeymaj80/my-ml-datasets/blob/master/screenshots/Congestion_Prediction_Page.png)
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # Importing packages
 import pandas as pd
 from src.components.create_custom_data import CreateCustomData
-from Flask import Flask, request, render_template, jsonify
+from src.components.make_predictions import MakePredictions
+from flask import Flask, request, render_template, jsonify
 
 # Creating the Flask app
 app = Flask(__name__)
@@ -24,8 +25,9 @@ def predict_datapoint():
     # Display the prediction landing page if the method is "GET"
     if request.method == 'GET':
         return render_template('predict.html')
-    else:
-        # If the method is "POST", then return the prediction
+    # If the method is "POST", then run the prediction
+    elif request.method == 'POST':
+        # Capturing the data entered by the user on the web app
         data = CreateCustomData(
             x = int(request.form.get('x')),
             y = int(request.form.get('y')),
@@ -34,4 +36,53 @@ def predict_datapoint():
         
         # Creating a dataframe from the user entered data
         df = data.create_dataframe()
+        
+        # Instantiating the prediction class and making the prediction
+        prediction = MakePredictions()
+        preds = prediction.predict(df)
+        
+        # Returning the prediction to the web app
+        return render_template('predict.html', results=preds, pred_df=df)
+
+# Creating a function to return the prediction as an API call
+def fetch_prediction_api():
+    '''
+    This function will take the data entered by the user and then 
+    return the prediction as an API call.
+    ==================================================================================
+    ---------------------
+    Returns:
+    ---------------------
+    predictions : json - This is the prediction in a JSON format.
+    ===================================================================================
+    '''
+    if request.method == 'POST':
+        # Capturing the data entered by the user on the web app
+        data = CreateCustomData(
+            x = int(request.form.get('x')),
+            y = int(request.form.get('y')),
+            direction = str(request.form.get('direction'))
+        )
+        
+        # Creating a dataframe from the user entered data
+        df = data.create_dataframe()
+        
+        # Instantiating the prediction class and making the prediction
+        prediction = MakePredictions()
+        preds = prediction.predict(df)
+        
+        # Creating a dictionary for the preds
+        preds_dict = {
+            'prediction': preds
+        }
+        
+        return jsonify(preds_dict)
+    
+# Running the Flask app
+if __name__ == '__main__':
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        print(f"Failed to run the Flask app: {e}")
+    
     
